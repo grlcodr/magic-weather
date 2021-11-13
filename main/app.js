f19c1bcf7bae636518a6d53b8b67e14a2798e4fc
grlcodr <rachael@grlcodr.dev>
2021-11-13 21:53:34 +0000

    added about

--- main/app.js
+++ main/app.js
@@ -8,13 +8,13 @@ let gif_div = document.querySelector('.rainbow-gif');
 let chooseText_div = document.querySelector('.choose-text');
 let weatherIcon_div = document.querySelector('.weather-icon');
 let time_p = document.querySelector('.time');
+let homeIcon = document.querySelector('.fa-fort-awesome');
 
 $('.dropdown-item').click(function() {
     let city = ( $(this).text() );
     city_div.style.display = 'block';
     gif_div.style.display = 'none';
     chooseText_div.style.display = 'none';
-    console.log(city);
     getResult(city);
 })
 
@@ -40,15 +40,10 @@ function displayResults(weather) {
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
-    weatherIcon_div.innerHTML = `<img src="icons/${weather.weather[0].icon}.png">`
+    weatherIcon_div.innerHTML = `<img src="/icons/${weather.weather[0].icon}.png">`
     
     let hilow = document.querySelector('.hi-low');
     hilow.innerText = `${Math.floor(weather.main.temp_min)}°f / ${Math.floor(weather.main.temp_max)}°f`;
-
-    const sunrise = new Date((weather.dt + weather.timezone) * 1000);
-    const hours = sunrise.getHours();
-    const minutes = sunrise.getMinutes()
-    time_p.innerText = `Local time: ${hours}:${minutes}`
 }
 
 function dateBuilder(d){
